0d5e5a317cb29a6cf92ee7e52c353b43d581da86
EunJI <83203198+ejalice@users.noreply.github.com>
2023-09-01 19:07:41 +0900

    Update 큰수의법칙 Answer

--- 01_Greedy/큰수의법칙.py
+++ 01_Greedy/큰수의법칙.py
@@ -1,21 +1,44 @@
+# N, M, K get input separated by space
 N, M, K = map(int, input().split())
 
-array = [0] * N
-sum = 0
-
+# Get N input separated by space
 array = list(map(int, input().split()))
-array.sort(reverse=True)
-print(array)
 
+array.sort() # sort
+first = array[N-1] # First Biggest
+second = array[N-2] # Second Biggest
 
-for j in range(1, M+1):
-    if (j%(K+1) == 0):
-      sum += array[1]
-      print(array[1], j, sum)
-      
-    else:
-       sum += array[0]
-       print(array[0], j, sum)
-      
+# Calculate the most biggest multiplied.
+firstMulti = int(M/(K+1)) * 3
+firstMulti += M%(K+1)
+
+sum = 0
+sum += firstMulti * first # First
+sum += (M - firstMulti) * second # Second
 
 print(sum)
+
+
+
+# N, M, K = map(int, input().split())
+
+# array = [0] * N
+# sum = 0
+
+# array = list(map(int, input().split()))
+# array.sort(reverse=True)
+# print(array)
+
+
+# for j in range(1, M+1):
+#     if (j%(K+1) == 0):
+#       sum += array[1]
+#       print(array[1], j, sum)
+      
+#     else:
+#        sum += array[0]
+#        print(array[0], j, sum)
+      
+
+# print(sum)
+
