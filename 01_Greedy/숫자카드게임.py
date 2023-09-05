da3436d8634d637769b265e85b075aaf4f39edec
EunJI <83203198+ejalice@users.noreply.github.com>
2023-09-05 14:25:18 +0900

    Update 숫자카드게임 Answer

--- 01_Greedy/숫자카드게임.py
+++ 01_Greedy/숫자카드게임.py
@@ -1,17 +1,30 @@
-# N: 행, M: 열
-N, M = map(int, input().split())
+# # N: 행, M: 열
+# N, M = map(int, input().split())
 
-# 2차원 배열
-data = []
-for i in range (N):
-    data.append(list(map(int, input().split())))
+# # 2차원 배열
+# data = []
+# for i in range (N):
+#     data.append(list(map(int, input().split())))
 
-for y in range(N):
-    if y == 0:
-        max = min(data[y])
-        print("y=0, max=%d" %max)
-    elif min(data[y-1]) < min(data[y]):
-        max = min(data[y])
-        print("y=%d, max=%d" %(y, max))
+# for y in range(N):
+#     if y == 0:
+#         max = min(data[y])
+#         print("y=0, max=%d" %max)
+#     elif min(data[y-1]) < min(data[y]):
+#         max = min(data[y])
+#         print("y=%d, max=%d" %(y, max))
 
-print(max)
+# print(max)
+
+# n, m get input separated by space
+n, m = map(int, input().split())
+
+result = 0
+for i in range(n):
+    data = list(map(int, input().split()))
+
+    # smallest in the row
+    min_value = min(data)
+    # biggest in the columns
+    result = max(result, min_value)
+print(result)
