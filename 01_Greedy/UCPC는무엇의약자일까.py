d1d30561ffc3d6d855d8b2466d1bb866ec7d895e
EunJI <83203198+ejalice@users.noreply.github.com>
2023-12-28 16:37:31 +0900

    Create BOJ_15904_UCPC는무엇의약자일까.py

---
+++ 01_Greedy/UCPC는무엇의약자일까.py
@@ -0,0 +1,41 @@
+# BOJ 15904번
+# Silver5
+
+complicated = input()
+upper = ""
+
+LOVE = "I love UCPC"
+HATE = "I hate UCPC"
+
+# 3. 대문자 비교 - O(n)
+def judge(upper):
+    i, j = 0, 0
+    length = len(upper)
+
+    while (i<length):
+        if upper[i] == "UCPC"[j]:
+            j += 1
+        i += 1
+        
+        if j == 4:
+            return LOVE
+    
+    return HATE
+
+# 1. 대문자만 가져오기 - O(n)
+for alpha in complicated:
+    if alpha.isupper():
+        upper += alpha
+
+# 2. 길이에 따라 case 나누기
+length = len(upper)
+
+if length < 4:
+    print(HATE)
+elif length == 4:
+    if upper == "UCPC":
+        print(LOVE)
+    else:
+        print(HATE)
+else:
+    print(judge(upper))
