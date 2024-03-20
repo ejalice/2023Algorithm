b37c82af2d874d7552254944748e9627a47242b6
EunJI <83203198+ejalice@users.noreply.github.com>
2024-03-20 09:53:49 +0900

    Update 이코테_2_1로만들기.py

--- DP/1로만들기.py
+++ DP/1로만들기.py
@@ -1,28 +1,50 @@
+# 방법2)
+import sys
 x = int(input())
-
+inf = sys.maxsize
 
 def toOne(x):
-    arr = [0] * 30000
-    arr[0] = x
+    table = [0] * (x+1)
 
-    for i in range(1, 30000):
-        past_x = arr[i-1]
+    for i in range(2, x+1):
+        minus_one = table[i-1] + 1
+        div_five = table[i//5]+1 if i%5 == 0 else inf
+        div_three = table[i//3]+1 if i%3 ==0 else inf
+        div_two = table[i//2]+1 if i%2 ==0 else inf
 
-        minus_one = past_x - 1
+        table[i] = min(minus_one, div_five, div_three, div_two)
 
-        if past_x%5 == 0:
-            div_five = past_x//5
-        else:
-            div_five = past_x
-
-        if past_x%3 == 0:
-            div_three = past_x//3
-        else:
-            div_three = past_x
-
-        arr[i] = min(minus_one, div_five, div_three)
-
-        if arr[i] == 1:
-            return i
+    return table[x]
+        
 
 print(toOne(x))
+
+# 방법1)
+# x = int(input())
+
+
+# def toOne(x):
+#     arr = [0] * 30000
+#     arr[0] = x
+
+#     for i in range(1, 30000):
+#         past_x = arr[i-1]
+
+#         minus_one = past_x - 1
+
+#         if past_x%5 == 0:
+#             div_five = past_x//5
+#         else:
+#             div_five = past_x
+
+#         if past_x%3 == 0:
+#             div_three = past_x//3
+#         else:
+#             div_three = past_x
+
+#         arr[i] = min(minus_one, div_five, div_three)
+
+#         if arr[i] == 1:
+#             return i
+
+# print(toOne(x))
