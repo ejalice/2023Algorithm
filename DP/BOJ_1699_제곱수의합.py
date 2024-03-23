9ab57552584eb1043d423d6df23f290441a878e5
EunJI <83203198+ejalice@users.noreply.github.com>
2024-03-23 23:22:36 +0900

    Update BOJ_1699_제곱수의합.py

--- DP/BOJ_1699_제곱수의합.py
+++ DP/BOJ_1699_제곱수의합.py
@@ -9,9 +9,7 @@ dp_table[0] = 0
 dp_table[1] = 1
 
 for i in range(2, N+1):
-    print(i, int(math.sqrt(i)+1))
     for j in range(1, int(math.sqrt(i)+1)):
-        
         dp_table[i] = min(dp_table[i], dp_table[i-j*j] + 1)
 
 print(dp_table[N])
